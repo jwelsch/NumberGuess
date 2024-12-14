ae2d1c621f80db56983da228b7402cdabfe5f2eb
Justin Welsch <j.welsch@gmail.com>
2024-12-14 13:35:56 -0500

    Initial commit

---
+++ src/NumberGuess/ApplicationModule.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NumberGuess.Common;
+
+namespace NumberGuess
+{
+    internal class ApplicationModule : Module
+    {
+        protected override void Load(IServiceCollection services, object[]? parameters = null)
+        {
+        }
+    }
+}
